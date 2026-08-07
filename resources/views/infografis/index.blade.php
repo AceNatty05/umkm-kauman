6045b5a0b40640b73f4396a725ab07415328fb2d
AceNatty05 <natasyacandrasari@gmail.com>
2026-08-08 02:41:37 +0700

    ubah teks di beranda

--- resources/views/infografis/index.blade.php
+++ resources/views/infografis/index.blade.php
@@ -24,7 +24,7 @@
     }">
         <div class="text-center mb-10">
             <h1 class="text-3xl font-bold text-kauman-primary">Infografis Desa Kauman</h1>
-            <p class="mt-2 text-gray-600">Kumpulan informasi dan poster terkait Desa Kauman dan UMKM lokal.</p>
+            <p class="mt-2 text-gray-600">Kumpulan informasi dan poster terkait pengelolaan UMKM</p>
         </div>
 
         @if($infografis->isEmpty())
